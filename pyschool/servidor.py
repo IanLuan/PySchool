7cbaaa674cd41810eca640ec5c3dc19621fb0f1f
Ian Luan <ianluan13@gmail.com>
2019-03-24 23:05:36 -0300

    update

--- pyschool/servidor.py
+++ pyschool/servidor.py
@@ -1,10 +1,17 @@
 from pessoa import Pessoa
 
 class servidor(Pessoa):
-    def __init__(self, nome, nascimento, sexo, rg, cpf, telefone, rua, bairro, numero, cep, cidade, estado, email, senha, estadoCivil, idPessoa, foto, cargo):
+    def __init__(self, nome, nascimento, sexo, rg, cpf, telefone, rua, bairro, numero, cep, cidade, estado, email, senha, estadoCivil, idPessoa, foto, adm, cargo):
         super().__init__(nome, nascimento, sexo, rg, cpf, telefone, rua, bairro, numero, cep, cidade, estado, email, senha, estadoCivil, idPessoa, foto)
+        self.__adm = adm
         seff.__cargo = cargo
     
+    def getAdm(self):
+        return self.__adm
+    
+    def setAdm(self, adm):
+        self.__adm = adm
+
     def getCargo(self):
         return self.__cargo
     
