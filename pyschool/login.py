ed1dd00c104eb3a5521336e3828d011aea5eb191
Ian Luan <ianluan13@gmail.com>
2019-03-25 02:29:14 -0300

    update

---
+++ pyschool/login.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'login.ui'
+#
+# Created by: PyQt5 UI code generator 5.12
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_background(object):
+    def setupUi(self, background):
+        background.setObjectName("background")
+        background.resize(350, 450)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Maximum, QtWidgets.QSizePolicy.Maximum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(background.sizePolicy().hasHeightForWidth())
+        background.setSizePolicy(sizePolicy)
+        background.setMaximumSize(QtCore.QSize(350, 450))
+        background.setStyleSheet("background-color: rgb(93, 115, 255)\n"
+"")
+        self.centralwidget = QtWidgets.QWidget(background)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(100, 64, 150, 77))
+        font = QtGui.QFont()
+        font.setFamily("Noto Sans")
+        font.setPointSize(22)
+        font.setBold(False)
+        font.setItalic(True)
+        font.setWeight(50)
+        self.label.setFont(font)
+        self.label.setStyleSheet("color: rgb(255, 255, 255);")
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName("label")
+        self.verticalWidget = QtWidgets.QWidget(self.centralwidget)
+        self.verticalWidget.setGeometry(QtCore.QRect(0, 146, 350, 107))
+        self.verticalWidget.setObjectName("verticalWidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalWidget)
+        self.verticalLayout.setContentsMargins(20, 0, 20, 0)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.lineEdit = QtWidgets.QLineEdit(self.verticalWidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Maximum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.lineEdit.sizePolicy().hasHeightForWidth())
+        self.lineEdit.setSizePolicy(sizePolicy)
+        self.lineEdit.setMinimumSize(QtCore.QSize(20, 0))
+        self.lineEdit.setMaximumSize(QtCore.QSize(16777215, 35))
+        self.lineEdit.setStyleSheet("QLineEdit { \n"
+"border: 5px solid white;\n"
+"border-radius: 15px;\n"
+"background-color: rgb(255, 255, 255);\n"
+"}")
+        self.lineEdit.setClearButtonEnabled(True)
+        self.lineEdit.setObjectName("lineEdit")
+        self.verticalLayout.addWidget(self.lineEdit)
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.verticalWidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Maximum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.lineEdit_2.sizePolicy().hasHeightForWidth())
+        self.lineEdit_2.setSizePolicy(sizePolicy)
+        self.lineEdit_2.setMaximumSize(QtCore.QSize(16777215, 35))
+        self.lineEdit_2.setStyleSheet("QLineEdit { \n"
+"border: 2px solid white;\n"
+"border-radius: 15px;\n"
+"background-color: rgb(255, 255, 255);\n"
+"}")
+        self.lineEdit_2.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.lineEdit_2.setClearButtonEnabled(True)
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.verticalLayout.addWidget(self.lineEdit_2)
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(22, 258, 307, 35))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(50)
+        self.pushButton.setFont(font)
+        self.pushButton.setStyleSheet("border: 2px solid white;\n"
+"border-radius: 15px;\n"
+"background-color: rgb(255, 255, 255);")
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(100, 294, 150, 25))
+        font = QtGui.QFont()
+        font.setPointSize(11)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(50)
+        self.pushButton_2.setFont(font)
+        self.pushButton_2.setStyleSheet("color: #fff")
+        self.pushButton_2.setAutoDefault(False)
+        self.pushButton_2.setDefault(False)
+        self.pushButton_2.setFlat(True)
+        self.pushButton_2.setObjectName("pushButton_2")
+        background.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(background)
+        QtCore.QMetaObject.connectSlotsByName(background)
+
+    def retranslateUi(self, background):
+        _translate = QtCore.QCoreApplication.translate
+        background.setWindowTitle(_translate("background", "MainWindow"))
+        self.label.setText(_translate("background", "Bem-vindo!"))
+        self.lineEdit.setPlaceholderText(_translate("background", "email"))
+        self.lineEdit_2.setPlaceholderText(_translate("background", "senha"))
+        self.pushButton.setText(_translate("background", "Entrar"))
+        self.pushButton_2.setText(_translate("background", "Esqueceu a senha?"))
+
+
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    background = QtWidgets.QMainWindow()
+    ui = Ui_background()
+    ui.setupUi(background)
+    background.show()
+    sys.exit(app.exec_())
