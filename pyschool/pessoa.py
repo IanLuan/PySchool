0df88fbff44560af5fdcae53c6a2107a8e754da4
Mayara Barbosa <mayara.barbosa2@hotmail.com>
2019-03-27 14:38:09 -0300

    Volta dos setters

--- pyschool/pessoa.py
+++ pyschool/pessoa.py
@@ -2,246 +2,115 @@
 class Pessoa:
 
     def __init__(self, nome, nascimento, sexo, rg, cpf, telefone, rua, bairro, numero, cep, cidade, estado, email, senha, estadoCivil, foto = None):
-        #Mudar tudo pra não obrigatório ou pra set?
-        self.nome = nome
-        self.nascimento = nascimento
-        self.sexo = sexo
-        self.rg = rg
-        self.cpf = cpf
-        self.telefone = telefone
-        self.rua = rua
-        self.bairro = bairro
-        self.numero = numero
-        self.cep = cep
-        self.cidade = cidade
-        self.estado = estado
-        self.email = email
-        self.senha = senha
-        self.estadoCivil = estadoCivil
-        self.foto = foto
+        self.setNome(nome)
+        self.setNascimento(nascimento)
+        self.setSexo(sexo)
+        self.setRg(rg)
+        self.setCpf(cpf)
+        self.setTelefone(telefone)
+        self.setRua(rua)
+        self.setBairro(bairro)
+        self.setNumero(numero)
+        self.setCep(cep)
+        self.setCidade(cidade)
+        self.setEstado(estado)
+        self.setEmail(email)
+        self.setSenha(senha)
+        self.setEstadoCivil(estadoCivil)
+        self.setFoto(foto)
 
-    @property
-    def nome(self):
+    def getNome(self):
         return self.__nome
 
-    @nome.setter
-    def nome(self, nome):
-        self.__nome = nome.capitalize()
+    def setNome(self, nome):
+        self.__nome = nome
 
-    @property
-    def nascimento(self):
+    def getNascimento(self):
         return self.__nascimento
 
-    @nascimento.setter
-    def nascimento(self, nascimento):
+    def setNascimento(self, nascimento):
         self.__nascimento = nascimento
 
-    @property
-    def sexo(self):
+    def getSexo(self):
         return self.__sexo
 
-    @sexo.setter
-    def sexo(self, sexo):
+    def setSexo(self, sexo):
         self.__sexo = sexo
 
-    @property
-    def rg(self):
+    def getRg(self):
         return self.__rg
 
-    @rg.setter
-    def rg(self, rg):
+    def setRg(self, rg):
         self.__rg = rg
 
-    @property
-    def cpf(self):
+    def getCpf(self):
         return self.__cpf
 
-    @cpf.setter
-    def cpf(self, cpf):
+    def setCpf(self, cpf):
         self.__cpf = cpf
 
-    @property
-    def telefone(self):
+    def getTelefone(self):
         return self.__telefone
 
-    @telefone.setter
-    def telefone(self, telefone):
+    def setTelefone(self, telefone):
         self.__telefone = telefone
 
-    @property
-    def rua(self):
+    def getRua(self):
         return self.__rua
 
-    @rua.setter
-    def rua(self, rua):
+    def setRua(self, rua):
         self.__rua = rua
 
-    @property
-    def bairro(self):
+    def getBairro(self):
         return self.__bairro
 
-    @bairro.setter
-    def bairro(self, bairro):
+    def setBairro(self, bairro):
         self.__bairro = bairro
 
-    @property
-    def numero(self):
+    def getNumero(self):
         return self.__numero
 
-    @numero.setter
-    def numero(self, numero):
+    def setNumero(self, numero):
         self.__numero = numero
 
-    @property
-    def cep(self):
+    def getCep(self):
         return self.__cep
 
-    @cep.setter
-    def cep(self, cep):
+    def setCep(self, cep):
         self.__cep = cep
 
-    @property
-    def cidade(self):
+    def getCidade(self):
         return self.__cidade
 
-    @cidade.setter
-    def cidade(self, cidade):
+    def setCidade(self, cidade):
         self.__cidade = cidade
 
-    @property
-    def estado(self):
+    def getEstado(self):
         return self.__estado
 
-    @estado.setter
-    def estado(self, estado):
+    def setEstado(self, estado):
         self.__estado = estado
 
-    @property
-    def email(self):
+    def getEmail(self):
         return self.__email
 
-    @email.setter
-    def email(self, email):
+    def setEmail(self, email):
         self.__email = email
 
-    @property
-    def senha(self):
+    def getSenha(self):
         return self.__senha
 
-    @senha.setter
-    def senha(self, senha):
+    def setSenha(self, senha):
         self.__senha = senha
 
-    @property
-    def estadoCivil(self):
+    def getEstadoCivil(self):
         return self.__estadoCivil
 
-    @estadoCivil.setter
-    def estadoCivil(self, estadoCivil):
+    def setEstadoCivil(self, estadoCivil):
         self.__estadoCivil = estadoCivil
 
-    @property
-    def foto(self):
+    def getFoto(self):
         return self.__foto
 
-    @foto.setter
-    def foto(self, foto):
+    def setFoto(self, foto):
         self.__foto = foto
-
-    """
-        def getNome(self):
-            return self.__nome
-
-        def setNome(self, nome):
-            self.__nome = nome
-
-        def getNascimento(self):
-            return self.__nascimento
-
-        def setNascimento(self, nascimento):
-            self.__nascimento = nascimento
-
-        def getSexo(self):
-            return self.__sexo
-
-        def setSexo(self, sexo):
-            self.__sexo = sexo
-
-        def getRg(self):
-            return self.__rg
-
-        def setRg(self, rg):
-            self.__rg = rg
-
-        def getCpf(self):
-            return self.__cpf
-
-        def setCpf(self, cpf):
-            self.__cpf = cpf
-
-        def getTelefone(self):
-            return self.__telefone
-
-        def setTelefone(self, telefone):
-            self.__telefone = telefone
-
-        def getRua(self):
-            return self.__rua
-
-        def setRua(self, rua):
-            self.__rua = rua
-
-        def getBairro(self):
-            return self.__bairro
-
-        def setBairro(self, bairro):
-            self.__bairro = bairro
-
-        def getNumero(self):
-            return self.__numero
-
-        def setNumero(self, numero):
-            self.__numero = numero
-
-        def getCep(self):
-            return self.__cep
-
-        def setCep(self, cep):
-            self.__cep = cep
-
-        def getCidade(self):
-            return self.__cidade
-
-        def setCidade(self, cidade):
-            self.__cidade = cidade
-
-        def getEstado(self):
-            return self.__estado
-
-        def setEstado(self, estado):
-            self.__estado = estado
-
-        def getEmail(self):
-            return self.__email
-
-        def setEmail(self, email):
-            self.__email = email
-
-        def getSenha(self):
-            return self.__senha
-
-        def setSenha(self, senha):
-            self.__senha = senha
-
-        def getEstadoCivil(self):
-            return self.__estadoCivil
-
-        def setEstadoCivil(self, estadoCivil):
-            self.__estadoCivil = estadoCivil
-
-        def getFoto(self):
-            return self.__foto
-
-        def setFoto(self, foto):
-            self.__foto = foto
-    """
--- pyschool/servidor.py
+++ pyschool/servidor.py
@@ -3,28 +3,24 @@ from pessoa import Pessoa
 #from pyschool.database import database
 from database import database
 
-
 class Servidor(Pessoa):
-    def __init__(self, nome, nascimento, sexo, rg, cpf, telefone, rua, bairro, numero, cep, cidade, estado, email, senha, estadoCivil, foto, adm, cargo):
+   def __init__(self, nome, nascimento, sexo, rg, cpf, telefone, rua, bairro, numero, cep, cidade, estado, email, senha, estadoCivil, foto, adm, cargo):
         super().__init__(nome, nascimento, sexo, rg, cpf, telefone, rua, bairro, numero, cep, cidade, estado, email, senha, estadoCivil, foto)
-        self.adm = adm
-        self.cargo = cargo
-
-    @property
-    def adm(self):
-        return self.__adm
-
-    @adm.setter
-    def adm(self, adm):
         self.__adm = adm
-
-    @property
-    def cargo(self):
-        return self.__cargo
-
-    @cargo.setter
-    def cargo(self, cargo):
         self.__cargo = cargo
+
+   def getAdm(self):
+       return self.__adm
+
+   def setAdm(self, adm):
+       self.__adm = adm
+
+   def getCargo(self):
+       return self.__cargo
+
+   def setCargo(self, cargo):
+       self.__cargo = cargo
+
 
     
     
