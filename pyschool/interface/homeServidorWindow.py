11963d91dc7ac13eeb33802d110217f014410dd6
Ian Luan <ianluan13@gmail.com>
2019-03-31 01:02:15 -0300

    Home Servidor

--- pyschool/interface/homeServidorWindow.py
+++ pyschool/interface/homeServidorWindow.py
@@ -12,20 +12,138 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1000, 550)
+        MainWindow.resize(680, 350)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
+        MainWindow.setSizePolicy(sizePolicy)
+        MainWindow.setMinimumSize(QtCore.QSize(680, 200))
+        MainWindow.setMaximumSize(QtCore.QSize(680, 400))
         self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setStyleSheet("#centralwidget {\n"
+"background-color:  rgb(21, 143, 181);\n"
+"}")
         self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(20, 20, 200, 90))
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setWeight(75)
+        self.pushButton.setFont(font)
+        self.pushButton.setStyleSheet("border: 2px solid rgb(255, 123, 28);\n"
+"border-radius: 15px;\n"
+"background-color: rgb(255, 123, 28);\n"
+"color: #fff;")
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("icons/matricula.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.pushButton.setIcon(icon)
+        self.pushButton.setIconSize(QtCore.QSize(32, 32))
+        self.pushButton.setFlat(False)
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(240, 20, 200, 90))
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setWeight(75)
+        self.pushButton_2.setFont(font)
+        self.pushButton_2.setStyleSheet("border: 2px solid rgb(255, 123, 28);\n"
+"border-radius: 15px;\n"
+"background-color: rgb(255, 123, 28);\n"
+"color: #fff;")
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap("icons/rematricula.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.pushButton_2.setIcon(icon1)
+        self.pushButton_2.setIconSize(QtCore.QSize(32, 32))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setGeometry(QtCore.QRect(460, 20, 200, 90))
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setWeight(75)
+        self.pushButton_3.setFont(font)
+        self.pushButton_3.setStyleSheet("border: 2px solid rgb(255, 123, 28);\n"
+"border-radius: 15px;\n"
+"background-color: rgb(255, 123, 28);\n"
+"color: #fff;")
+        icon2 = QtGui.QIcon()
+        icon2.addPixmap(QtGui.QPixmap("icons/alunos.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.pushButton_3.setIcon(icon2)
+        self.pushButton_3.setIconSize(QtCore.QSize(32, 32))
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_4.setGeometry(QtCore.QRect(460, 130, 200, 90))
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setWeight(75)
+        self.pushButton_4.setFont(font)
+        self.pushButton_4.setStyleSheet("border: 2px solid rgb(255, 123, 28);\n"
+"border-radius: 15px;\n"
+"background-color: rgb(255, 123, 28);\n"
+"color: #fff;")
+        icon3 = QtGui.QIcon()
+        icon3.addPixmap(QtGui.QPixmap("icons/materias.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.pushButton_4.setIcon(icon3)
+        self.pushButton_4.setIconSize(QtCore.QSize(32, 32))
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.pushButton_5 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_5.setGeometry(QtCore.QRect(240, 130, 200, 90))
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setWeight(75)
+        self.pushButton_5.setFont(font)
+        self.pushButton_5.setStyleSheet("border: 2px solid rgb(255, 123, 28);\n"
+"border-radius: 15px;\n"
+"background-color: rgb(255, 123, 28);\n"
+"color: #fff;")
+        self.pushButton_5.setIcon(icon2)
+        self.pushButton_5.setIconSize(QtCore.QSize(32, 32))
+        self.pushButton_5.setObjectName("pushButton_5")
+        self.pushButton_6 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_6.setGeometry(QtCore.QRect(20, 130, 200, 90))
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setWeight(75)
+        self.pushButton_6.setFont(font)
+        self.pushButton_6.setStyleSheet("border: 2px solid rgb(255, 123, 28);\n"
+"border-radius: 15px;\n"
+"background-color: rgb(255, 123, 28);\n"
+"color: #fff;")
+        icon4 = QtGui.QIcon()
+        icon4.addPixmap(QtGui.QPixmap("icons/turma.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.pushButton_6.setIcon(icon4)
+        self.pushButton_6.setIconSize(QtCore.QSize(32, 32))
+        self.pushButton_6.setObjectName("pushButton_6")
+        self.pushButton_9 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_9.setGeometry(QtCore.QRect(20, 240, 200, 90))
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setWeight(75)
+        self.pushButton_9.setFont(font)
+        self.pushButton_9.setStyleSheet("border: 2px solid rgb(255, 123, 28);\n"
+"border-radius: 15px;\n"
+"background-color: rgb(255, 123, 28);\n"
+"color: #fff;")
+        icon5 = QtGui.QIcon()
+        icon5.addPixmap(QtGui.QPixmap("icons/person.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.pushButton_9.setIcon(icon5)
+        self.pushButton_9.setIconSize(QtCore.QSize(32, 32))
+        self.pushButton_9.setObjectName("pushButton_9")
         MainWindow.setCentralWidget(self.centralwidget)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Início"))
+        self.pushButton.setText(_translate("MainWindow", "Matrícula"))
+        self.pushButton_2.setText(_translate("MainWindow", "Rematrícula"))
+        self.pushButton_3.setText(_translate("MainWindow", "Alunos"))
+        self.pushButton_4.setText(_translate("MainWindow", "Matérias"))
+        self.pushButton_5.setText(_translate("MainWindow", "Professores"))
+        self.pushButton_6.setText(_translate("MainWindow", "Turmas"))
+        self.pushButton_9.setText(_translate("MainWindow", "Perfil do Servidor"))
 
 
 
