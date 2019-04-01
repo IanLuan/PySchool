e82d5b4cd00c709e9fd9a0b445472b505a20c311
Ian Luan <ianluan13@gmail.com>
2019-04-01 02:09:39 -0300

    mais connects

---
+++ pyschool/verCargos.py
@@ -0,0 +1,52 @@
+import sys
+from functools import partial
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from database.database import Database
+from interface.verCargosWindow import *
+
+import cadastroCargo
+import homeServidor
+import homeAdm
+
+
+# tela
+app = QtWidgets.QApplication(sys.argv)
+MainWindow = QtWidgets.QMainWindow()
+tela = Ui_verCargos()
+tela.setupUi(MainWindow)
+
+
+def cadastrarCargos(id):
+	cadastroCargo.startCadastroCargo(id)
+
+def voltarHome(id, type):
+	MainWindow.close()
+	homeAdm.startHomeAdm(id)
+
+def adicionarCargos():
+
+    cargos = ["professor", "adm", "secretário"]
+
+    for cargo in cargos:
+        tela.model.appendRow(QStandardItem(cargo))
+
+def startCargos(id):
+
+    # Configurar tabela
+    tela.model = QStandardItemModel()  
+    tela.tableCargos.setModel(tela.model)
+    tela.model.setHorizontalHeaderLabels(['Cargos'])
+    tela.tableCargos.setSelectionBehavior(QAbstractItemView.SelectRows)
+    tela.tableCargos.setColumnWidth(0, 350)
+    adicionarCargos()
+
+    # Cadastrar Cargo
+    tela.btnCargo.clicked.connect(partial(cadastrarCargos, id))
+
+    # Voltar
+    tela.btnVoltar.clicked.connect(partial(voltarHome, id))
+
+
+    # run
+    MainWindow.show()
